6c6d64cf07597980e2167790d99be99df1d59a5b
Szak0 <sattilabence@gmail.com>
2020-09-29 19:44:45 +0200

    Filtering parts moved here

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from "react";
 import "./App.css";
 import { GamesProvider } from "./components/contexts/GamesContext";
 import GamesList from "./components/GamesList";
+import Filters from "./components/Filters";
+import SearchBar from "./components/SearchBar";
+import DateFilter from "./components/DateFilter";
 
 function App() {
   return (
     <GamesProvider>
       <div className="App">
+        <SearchBar />
+        <Filters />
+        <DateFilter />
         <GamesList />
       </div>
     </GamesProvider>
