ba6aabb64474956c4d79047ca1a429a076eb2917
Szak0 <sattilabence@gmail.com>
2020-09-30 10:47:29 +0200

    Added routes

--- src/App.js
+++ src/App.js
@@ -7,19 +7,34 @@ import SearchBar from "./components/SearchBar";
 import DateFilter from "./components/DateFilter";
 import Pagination from "./components/Pagination";
 import Header from "./Header";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <GamesProvider>
-      <Header />
-      <div className="App">
-        <SearchBar />
-        <Filters />
-        <DateFilter />
-        <GamesList />
-        <Pagination />
-      </div>
-    </GamesProvider>
+    <React.Fragment>
+      <BrowserRouter>
+        <GamesProvider>
+          <CssBaseline />
+          <Header />
+          <div className="App">
+            <SearchBar />
+            <Filters />
+            <DateFilter />
+            <Switch>
+              <Route path={"/"} exact={true} component={GamesList}>
+                <GamesList />
+              </Route>
+              <Route path="/game/:id">
+                <GamesList />
+              </Route>
+            </Switch>
+
+            <Pagination />
+          </div>
+        </GamesProvider>
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
 
