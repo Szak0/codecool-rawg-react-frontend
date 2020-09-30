185769dc6f9d5cd4bb3204faf2382f0311b7d15a
Balint Ligeti <balint.ligeti@hotmail.com>
2020-09-30 21:05:38 +0200

    Platforms logos +done+

--- src/components/GameDetails.jsx
+++ src/components/GameDetails.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
+import Platforms from "./Platforms"
 
 const GameDetails = () => {
 
-    const [game, setGame] = useState([])
+    const [game, setGame] = useState([]);
+    const [platforms, setPlatforms] = useState([])
   let id = window.location.href.split('/').reverse()[0];
 
   useEffect(() => {
@@ -14,19 +15,23 @@ const GameDetails = () => {
       );
       
       setGame(request.data);
+      setPlatforms(request.data.platforms);
     };
     fetchData();
-  });
+  }, []);
 
 
-  
 
   return (
     <div >
         <h1>{game.name}</h1>
         <img src={game.background_image} alt="" style={{width: "20vw"}}/>
         <p>{game.released}</p> 
-      <p>{game.description_raw}</p>      
+      <p>{game.description_raw}</p> 
+      <p>
+        
+        <Platforms platforms={platforms}/>
+      </p>
     </div>
   );
 };
