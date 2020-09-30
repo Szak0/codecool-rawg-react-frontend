68a60079335ecad08de415595505ca3487e2adbc
Szak0 <sattilabence@gmail.com>
2020-09-30 16:14:07 +0200

    Serves an SVG logo

---
+++ src/components/UI/PopularLogo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PopularLogo = () => {
+    return (
+        <div>
+            <svg class="side-logo-svg" xmlns="http://www.w3.org/2000/svg" width="24" height="16" viewBox="0 0 24 16"><path fill="black" fill-rule="evenodd" d="M0 4h6v12H0V4zm9-4h6v16H9V0zm9 6h6v10h-6V6z"></path></svg>
+        </div>
+    )
+}
+
+export default PopularLogo
