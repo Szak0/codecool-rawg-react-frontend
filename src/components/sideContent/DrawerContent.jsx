eb906b06905ac44282a46df41eccf4894b632220
Szak0 <sattilabence@gmail.com>
2020-10-12 10:16:35 +0200

    Drawer now has multiply items

--- src/components/sideContent/DrawerContent.jsx
+++ src/components/sideContent/DrawerContent.jsx
@@ -14,7 +14,9 @@ import NextWeekLogo from "../UI/NextWeekLogo";
 import StarLogo from "../UI/StarLogo";
 
 const DrawerContent = () => {
-  const [data, games, filters, setFilters] = useContext(GamesContext);
+  const [data, games, filters, setFilters, isLoading, setGames] = useContext(
+    GamesContext
+  );
 
   const handleAllTimeTop = () => {
     setFilters({
@@ -22,6 +24,7 @@ const DrawerContent = () => {
       ordering: "-rating",
       dates: "1920-01-01,2020-01-01",
     });
+    setGames([]);
   };
   const handlePopularLastYear = () => {
     setFilters({
@@ -29,6 +32,7 @@ const DrawerContent = () => {
       ordering: "-rating",
       dates: "2019-01-01,2020-01-01",
     });
+    setGames([]);
   };
   const handleBestOfTheYear = () => {
     setFilters({
@@ -36,13 +40,42 @@ const DrawerContent = () => {
       ordering: "-rating",
       dates: "2020-01-01,2020-12-31",
     });
+    setGames([]);
+  };
+  const handleNextWeek = () => {
+    setFilters({
+      page: 1,
+      ordering: "-added",
+      dates: "2020-10-05,2020-10-11",
+    });
+    setGames([]);
+  };
+  const handleThisWeek = () => {
+    setFilters({
+      page: 1,
+      ordering: "-added",
+      dates: "2020-09-28,2020-10-04",
+    });
+  };
+  const handleLast30Days = () => {
+    setFilters({
+      page: 1,
+      ordering: "-added",
+      dates: "2020-09-02,2020-10-02",
+    });
+    setGames([]);
   };
   return (
     <div>
       <List>
+        <ListItem button>
+          <ListItemText primary={"Home"} />
+        </ListItem>
+        <Divider />
         <ListItem>
           <ListItemText primary={"Tops"} />
         </ListItem>
+
         <Link
           to="/all-time-top"
           className="normalize-link"
@@ -79,46 +112,49 @@ const DrawerContent = () => {
             <ListItemText primary={"Popular in 2019"} />
           </ListItem>
         </Link>
+        <Divider />
+        <ListItem>
+          <ListItemText primary={"New Releases"} />
+        </ListItem>
+
+        <Link
+          to="/last-30-days"
+          className="normalize-link"
+          onClick={handleLast30Days}
+        >
+          <ListItem button>
+            <ListItemIcon>
+              <StarLogo />
+            </ListItemIcon>
+            <ListItemText primary={"Last 30 days"} />
+          </ListItem>
+        </Link>
+        <Link
+          to="/this-week"
+          className="normalize-link"
+          onClick={handleThisWeek}
+        >
+          <ListItem button>
+            <ListItemIcon>
+              <ThisWeekLogo />
+            </ListItemIcon>
+            <ListItemText primary={"This Week"} />
+          </ListItem>
+        </Link>
+        <Link
+          to="/next-week"
+          className="normalize-link"
+          onClick={handleNextWeek}
+        >
+          <ListItem button>
+            <ListItemIcon>
+              <NextWeekLogo />
+            </ListItemIcon>
+            <ListItemText primary={"Next Week"} />
+          </ListItem>
+        </Link>
+        <Divider />
       </List>
-      <ListItem>
-        <ListItemText primary={"New Releases"} />
-      </ListItem>
-      <Link
-        to="/last-30-days"
-        className="normalize-link"
-        onClick={handleAllTimeTop}
-      >
-        <ListItem button>
-          <ListItemIcon>
-            <StarLogo />
-          </ListItemIcon>
-          <ListItemText primary={"Last 30 days"} />
-        </ListItem>
-      </Link>
-      <Link
-        to="/this-week"
-        className="normalize-link"
-        onClick={handleAllTimeTop}
-      >
-        <ListItem button>
-          <ListItemIcon>
-            <ThisWeekLogo />
-          </ListItemIcon>
-          <ListItemText primary={"This Week"} />
-        </ListItem>
-      </Link>
-      <Link
-        to="/next-week"
-        className="normalize-link"
-        onClick={handleAllTimeTop}
-      >
-        <ListItem button>
-          <ListItemIcon>
-            <NextWeekLogo />
-          </ListItemIcon>
-          <ListItemText primary={"Next Week"} />
-        </ListItem>
-      </Link>
     </div>
   );
 };
