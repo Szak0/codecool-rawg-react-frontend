19e7ae89de658f89636b2c7c27eb82f501100c45
Szak0 <sattilabence@gmail.com>
2020-10-23 14:58:46 +0200

    Added index to key

--- src/components/game-cards/GamesList.jsx
+++ src/components/game-cards/GamesList.jsx
@@ -1,22 +1,14 @@
-import React, {
-  useContext,
-  useRef,
-  useCallback,
-} from "react";
+import React, { useContext, useRef, useCallback } from "react";
 import { GamesContext } from "../contexts/GamesContext";
 import GameItem from "./GameItem";
 import LoadingRing from "../UI/loading-ring/LoadingRing";
+import Filters from "../UI/filters/ordering-filter/Filters";
+import DateFilter from "../UI/filters/date-filter/DateFilter";
 
 const GamesList = () => {
-  const [
-    data,
-    games,
-    filters,
-    setFilters,
-    isLoading,
-    setGames,
-    isError,
-  ] = useContext(GamesContext);
+  const [data, games, filters, setFilters, isLoading] = useContext(
+    GamesContext
+  );
 
   const observer = useRef();
   const lastGameCardRef = useCallback(
@@ -37,45 +29,30 @@ const GamesList = () => {
     },
     [filters, isLoading, setFilters]
   );
-
-  function numberFormat(n) {
-    return String(n).replace(/(.)(?=(\d{3})+$)/g, "$1,");
-  }
-
   return (
-    <div>
-      <h2>About {numberFormat(data.count)} results</h2>
-      <h1>{data.seo_title}</h1>
+    <div className={"flex-container"}>
       <div className={"game-list-container"}>
         <section className="games-container">
           {games.map((game, index) => {
             if (games.length === index + 1) {
               return (
-                <div key={game.name + game.id + "-a"} ref={lastGameCardRef}>
+                <div
+                  key={game.name + game.id + "-a" + index}
+                  ref={lastGameCardRef}
+                >
                   <GameItem game={game} />
                 </div>
               );
             } else
-              return <GameItem game={game} key={game.name + game.id + "-b"} />;
+              return (
+                <GameItem
+                  game={game}
+                  key={game.name + game.id + "-b" + index}
+                />
+              );
           })}
         </section>
       </div>
-
-      {isError ? (
-        <h6
-          style={{
-            fontSize: "38px",
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
-          No more to display
-        </h6>
-      ) : isLoading ? (
-        <LoadingRing />
-      ) : null}
-
-      <div></div>
     </div>
   );
 };
