dfd3498f09b810d272346bcb39f91c2dfe400b38
Szak0 <sattilabence@gmail.com>
2020-10-23 14:55:36 +0200

    Axios using client.get to config request

--- src/components/contexts/GamesContext.jsx
+++ src/components/contexts/GamesContext.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState, createContext } from "react";
 import axios from "axios";
 import queryString from "query-string";
-import { loadProgressBar } from 'axios-progress-bar'
-import 'axios-progress-bar/dist/nprogress.css'
+
 export const GamesContext = createContext();
 
 export const GamesProvider = (props) => {
@@ -22,29 +21,24 @@ export const GamesProvider = (props) => {
   });
 
   useEffect(() => {
-    const paramString = queryString.stringify(filters);
-    console.log(paramString, "param string");
     const fetchData = async () => {
       setIsError(null);
       setIsLoading(true);
       try {
         const client = axios.create({
-          baseURL:  `http://localhost:8080/api`,
-          timeout: 20000
-        })
-        const request = await client.get('/games?' + paramString, {
-          onDownloadProgress: progressEvent => {
-            console.log(progressEvent.loaded);
-            console.log(progressEvent);
-            let percentCompleted = Math.floor(progressEvent.loaded / progressEvent.total * 100)
-            console.log('completed: ', percentCompleted)
-          }});
-       console.log(request);
-        if (request.data.results){
+          baseURL: `https://api.rawg.io/api`,
+          timeout: 20000,
+        });
+        const request = await client.get(
+          "/games?" + queryString.stringify(filters)
+        );
+        if (request.data.results) {
           setGames((game) => [...game, ...request.data.results]);
         } else {
           setIsError(true);
         }
+        console.log("/games?" + queryString.stringify(filters));
+        console.log(request.data);
         setData(request.data);
         setIsLoading(false);
       } catch (error) {
