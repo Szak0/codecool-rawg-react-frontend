67d98fc09ea9958881117a05f78556f5340b3c6b
Szak0 <sattilabence@gmail.com>
2020-10-29 20:31:10 +0100

    Checks for email/password availability

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -17,6 +17,7 @@ const Register = () => {
   const [userPassword, setUserPassword] = useState("");
   const [passwordConfirmation, setPasswordConfirmation] = useState("");
   const [error, setError] = useState(false);
+  const [authServerDown, setAuthServerDown] = useState(false);
 
   const passwordHasing = async (plainPassword) => {
     bcrypt.genSalt(saltRounds, function (err, salt) {
@@ -27,9 +28,7 @@ const Register = () => {
           if (err) {
             throw err;
           } else {
-              sendRequest(hash);
-            console.log(hash);
-               
+            sendRequest(hash);
           }
         });
       }
@@ -37,106 +36,127 @@ const Register = () => {
   };
 
   const sendRequest = (hash) => {
-    try {
-      const baseURL = `http://localhost:8080/api/register`;
-      axios.post(baseURL, {
-        userName: userName,
-        email: userEmail,
-        password: hash
-      });
-    } catch (error) {
-      setError(true);
-    }
-    
+    axios.post(`http://localhost:8080/api/register`, {
+      userName: userName,
+      email: userEmail,
+      password: hash
+    }).then(res => {
+      console.log("Ok");
+    })
+      .catch(err => {
+        if (err.response) {
+          // client received an error response (5xx, 4xx)
+          console.log("error");
+          setError(true);
+        } else if (err.request) {
+          // client never received a response, or request never left
+          setAuthServerDown(true)
+        } else {
+          // anything else
+        }
+      })
   }
-
-
 
   const handlePost = (event) => {
     event.preventDefault();
     if (validateForm()) {
       passwordHasing(userPassword)
-    } 
+    }
   };
 
   function validateForm() {
     const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
     if (userEmail && userPassword && userName) {
       if (userEmail.match(emailRegex)) {
-        if (userEmail.length > 0 
-          && userPassword.length >= 8 
-          && userName.length > 3){
+        if (userEmail.length > 0
+          && userPassword.length >= 8
+          && userName.length > 3) {
           return validateMatchingPasswords();
         }
-     else {
-       return false;
+        else {
+          return false;
+        }
+      }
     }
   }
-}
-}
 
-  function validateMatchingPasswords(){
+  function validateMatchingPasswords() {
     return userPassword == passwordConfirmation;
     // return userPassword.match(passwordConfirmation);   
   }
   const handleEmail = (e) => {
+    setError(false);
     setUserEmail(e.target.value);
   };
 
   const handleUserName = (e) => {
+    setError(false);
     setUserName(e.target.value);
   };
 
   const handlePassword = (e) => {
     e.preventDefault();
-    console.log("Az első " + e.target.value);
+    setError(false);
     setUserPassword(e.target.value);
   };
 
   const handlePasswordConfirmation = (e) => {
     e.preventDefault();
+    setError(false);
     setPasswordConfirmation(e.target.value);
   };
 
   return (
-    <div>
-      <h2>{error}</h2>
+    <div className={"registration-form"}>
       <form>
-        <FormControl>
-          <InputLabel htmlFor="my-input">Email address</InputLabel>
-          <Input
-            id="email"
-            aria-describedby="my-helper-text"
-            onChange={handleEmail}
-          />
-        </FormControl>
-        <FormControl>
-          <InputLabel htmlFor="my-input">User name</InputLabel>
-          <Input
-            id="user-name"
-            aria-describedby="my-helper-text"
-            onChange={handleUserName}
-          />
-        </FormControl>
-        <FormControl>
-          <InputLabel htmlFor="my-input">Password</InputLabel>
-          <Input
-            id="password"
-            aria-describedby="my-helper-text"
-            onChange={handlePassword}
-          />
-        </FormControl>
-        <FormControl>
-          <InputLabel htmlFor="my-input">Confirm Password</InputLabel>
-          <Input
-            id="password-conf"
-            aria-describedby="my-helper-text"
-            onChange={handlePasswordConfirmation}
-          />
-        </FormControl>
-        <Input type="submit" value="submit" onClick={handlePost}  />
+        <div>
+          {error ? <h1>This username/email already in use!</h1> : <h1>Come to join us!</h1>}
+        </div>
+        <div className={'forms'}>
+          <FormControl>
+            <InputLabel htmlFor="my-input">Email address</InputLabel>
+            <Input
+              id="email"
+              aria-describedby="my-helper-text"
+              onChange={handleEmail}
+            />
+          </FormControl>
+        </div>
+        <div className={'forms'}>
+          <FormControl>
+            <InputLabel htmlFor="my-input">User name</InputLabel>
+            <Input
+              id="user-name"
+              aria-describedby="my-helper-text"
+              onChange={handleUserName}
+            />
+          </FormControl>
+        </div>
+        <div className={'forms'}>
+          <FormControl>
+            <InputLabel htmlFor="my-input">Password</InputLabel>
+            <Input
+              id="password"
+              aria-describedby="my-helper-text"
+              onChange={handlePassword}
+            />
+          </FormControl>
+        </div>
+        <div className={'forms'}>
+          <FormControl>
+            <InputLabel htmlFor="my-input">Confirm Password</InputLabel>
+            <Input
+              id="password-conf"
+              aria-describedby="my-helper-text"
+              onChange={handlePasswordConfirmation}
+            />
+          </FormControl>
+        </div>
+        <div className={'forms'}>
+          <Input type="submit" value="submit" onClick={handlePost} />
+        </div>
       </form>
-      {error ? <h1>This username/email already in use!</h1> : null}
+
     </div>
   );
 };
